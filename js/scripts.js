8af760c39f98bfc4c04c05fa65eae838771ea5b7
Staged Files <>
2019-03-04 19:27:38 -0600

    js query

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi there! Welcome to my portfolio!")
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
